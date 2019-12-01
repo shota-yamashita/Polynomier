9aeba12a507c542fa80c052458ff9e5f99b2774b
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2019-12-01 17:39:34 +0900

    Fix bug

--- main.js
+++ main.js
@@ -37,14 +37,14 @@ function buildPolynomial(degree) {
     switch(degree) {
         case 0:
             var axTerm = getRandomIntInclusive(min=0, max=4);
-            if (a === 1) { a = ""; }
+            if (a === 1 && axTerm <= 1) { a = ""; }
             polynomial = `${a}${xSign(axTerm)}`;
             break;
         case 1:
             var axTerm = getRandomIntInclusive(min=1, max=4);
             var bxTerm = getRandomIntInclusive(min=0, max=axTerm - 1);
             if (a === 1) { a = ""; }
-            if (b === 1) { b = ""; }
+            if (b === 1 && bxTerm <= 1) { b = ""; }
             polynomial = `${a}${xSign(axTerm)} ${sign()} ${b}${xSign(bxTerm)}`;
             break;
         case 2:
@@ -53,7 +53,7 @@ function buildPolynomial(degree) {
             var cxTerm = getRandomIntInclusive(min=0, max=bxTerm - 1);
             if (a === 1) { a = ""; }
             if (b === 1) { b = ""; }
-            if (c === 1) { c = ""; }
+            if (c === 1 && cxTerm <= 1) { c = ""; }
             polynomial = `${a}${xSign(axTerm)} ${sign()} ${b}${xSign(bxTerm)} ${sign()} ${c}${xSign(cxTerm)}`;
             break;
         case 3:
@@ -64,7 +64,7 @@ function buildPolynomial(degree) {
             if (a === 1) { a = ""; }
             if (b === 1) { b = ""; }
             if (c === 1) { c = ""; }
-            if (d === 1) { d = ""; }
+            if (d === 1 && dxTerm <= 1) { d = ""; }
             polynomial = `${a}${xSign(axTerm)} ${sign()} ${b}${xSign(bxTerm)} ${sign()} ${c}${xSign(cxTerm)} ${sign()} ${d}${xSign(dxTerm)}`;
             break;
     }
