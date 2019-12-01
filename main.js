cb9cf97c77052dbe30fe10f87a81f441f68cf2c2
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2019-12-01 18:11:22 +0900

    add polynomial petterns

--- main.js
+++ main.js
@@ -34,17 +34,22 @@ function buildPolynomial(degree) {
     var polynomial;
     var integersCount = degree + 1;
     var [a, b, c, d] = getIntegers(integersCount);
+
+    if ( getRandomIntInclusive(min=1, max=2) === 1) {
+        a = a * (-1);
+    }
+
     switch(degree) {
         case 0:
             var axTerm = getRandomIntInclusive(min=0, max=4);
-            if (a === 1 && axTerm <= 1) { a = ""; }
+            if ([1, -1].includes(a) && axTerm <= 1) { a = ""; }
             polynomial = `${a}${xSign(axTerm)}`;
             break;
         case 1:
             var axTerm = getRandomIntInclusive(min=1, max=4);
             var bxTerm = getRandomIntInclusive(min=0, max=axTerm - 1);
             if (a === 1) { a = ""; }
-            if (b === 1 && bxTerm <= 1) { b = ""; }
+            if (bxTerm === 1) { b = ""; }
             polynomial = `${a}${xSign(axTerm)} ${sign()} ${b}${xSign(bxTerm)}`;
             break;
         case 2:
@@ -53,7 +58,7 @@ function buildPolynomial(degree) {
             var cxTerm = getRandomIntInclusive(min=0, max=bxTerm - 1);
             if (a === 1) { a = ""; }
             if (b === 1) { b = ""; }
-            if (c === 1 && cxTerm <= 1) { c = ""; }
+            if (cxTerm === 1) { c = ""; }
             polynomial = `${a}${xSign(axTerm)} ${sign()} ${b}${xSign(bxTerm)} ${sign()} ${c}${xSign(cxTerm)}`;
             break;
         case 3:
@@ -64,7 +69,7 @@ function buildPolynomial(degree) {
             if (a === 1) { a = ""; }
             if (b === 1) { b = ""; }
             if (c === 1) { c = ""; }
-            if (d === 1 && dxTerm <= 1) { d = ""; }
+            if (dxTerm === 1) { d = ""; }
             polynomial = `${a}${xSign(axTerm)} ${sign()} ${b}${xSign(bxTerm)} ${sign()} ${c}${xSign(cxTerm)} ${sign()} ${d}${xSign(dxTerm)}`;
             break;
     }
